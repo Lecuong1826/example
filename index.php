3e1b41a4a18e298349fb6fd91f4077f5f0884650
Lecuong1826 <109645657+Lecuong1826@users.noreply.github.com>
2022-07-27 14:35:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php include_once("home.html"); ?>
+<?php
+    $myfile = fopen("names.txt", "w");
+    
+    $txt = "John\n";
+    fwrite($myfile, $txt);
+    $txt = "David\n";
+    fwrite($myfile, $txt);
+    
+    fclose($myfile);
+    
+    /* Nộ dung file:
+    John
+    David
+    */
+?>
